db70851464bb8fe16104c916d25fffe5ff91d928
Abdul Gamal <abdul.nanjira@gmail.com>
2023-03-07 18:51:43 +0500

    initial commit

---
+++ src/pages/add-new/index.js
@@ -0,0 +1,141 @@
+import Link from "next/link";
+import React from "react";
+
+function index() {
+  return (
+    <div className="mx-auto max-w-screen-xl px-4 py-16 sm:px-6 lg:px-8">
+      <div className="mx-auto md:w-[80%] w-full">
+        <h1 className="text-center text-2xl font-bold text-gray-600 sm:text-3xl">
+          New Product
+        </h1>
+
+        <form className="mt-6 mb-0 space-y-4 rounded-lg p-4 shadow-lg sm:p-6 lg:p-8">
+          <div class="grid gap-6 mb-6 md:grid-cols-2">
+            <div>
+              <label
+                htmlFor="first_name"
+                class="block mb-2 text-sm font-medium text-gray-600"
+              >
+                Product name
+              </label>
+              <input
+                type="text"
+                id="first_name"
+                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+                placeholder="Bag"
+                required
+              />
+            </div>
+            <div>
+              <label
+                htmlFor="countries"
+                className="block mb-2 text-sm font-medium text-gray-600"
+              >
+                Type of payment
+              </label>
+              <select
+                id="countries"
+                className="bg-gray-50 border border-gray-300 text-gray-600 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+              >
+                <option value="split" selected>
+                  Split
+                </option>
+                <option value="one-time">One-time</option>
+              </select>
+            </div>
+            <div>
+              <label
+                htmlFor="category"
+                className="block mb-2 text-sm font-medium text-gray-600"
+              >
+                Category
+              </label>
+              <input
+                type="text"
+                id="category"
+                className="bg-gray-50 border border-gray-300 text-gray-600 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+                placeholder="Fashion"
+                required
+              />
+            </div>
+            <div>
+              <label
+                htmlFor="price"
+                className="block mb-2 text-sm font-medium text-gray-600"
+              >
+                Price (KES)
+              </label>
+              <div class="flex">
+                <span class="inline-flex items-center px-3 text-sm text-gray-600 bg-gray-200 border border-r-0 border-gray-300 rounded-l-md">
+                  $
+                </span>
+                <input
+                  type="text"
+                  id="price"
+                  class="rounded-none rounded-r-lg bg-gray-50 border text-gray-600 focus:ring-blue-500 focus:border-blue-500 block flex-1 min-w-0 w-full text-sm border-gray-300 p-2.5"
+                  placeholder="20"
+                />
+              </div>
+            </div>
+            <div className="flex items-center justify-center w-full">
+              <label
+                for="dropzone-file"
+                className="flex flex-col items-center justify-center w-full h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 hover:bg-gray-100"
+              >
+                <div className="flex flex-col items-center justify-center pt-5 pb-6">
+                  <svg
+                    aria-hidden="true"
+                    className="w-10 h-10 mb-3 text-gray-400"
+                    fill="none"
+                    stroke="currentColor"
+                    viewBox="0 0 24 24"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="M7 16a4 4 0 01-.88-7.903A5 5 0 1115.9 6L16 6a5 5 0 011 9.9M15 13l-3-3m0 0l-3 3m3-3v12"
+                    ></path>
+                  </svg>
+                  <p className="mb-2 text-sm text-gray-500">
+                    <span className="font-semibold">Click to upload</span> or
+                    drag and drop
+                  </p>
+                  <p className="text-xs text-gray-500">
+                    SVG, PNG, JPG or GIF (MAX. 800x400px)
+                  </p>
+                </div>
+                <input id="dropzone-file" type="file" class="hidden" />
+              </label>
+            </div>
+            <div>
+              <label
+                htmlFor="description"
+                class="block mb-2 text-sm font-medium text-gray-600"
+              >
+                Product description
+              </label>
+              <textarea
+                type="text"
+                id="description"
+                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+                placeholder="Product description..."
+                required
+              />
+            </div>
+          </div>
+
+          <button
+            type="submit"
+            class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+          >
+            <Link href="/">Submit</Link>
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default index;
---
+++ src/pages/sign-up/index.js
@@ -0,0 +1,316 @@
+import Link from "next/link";
+import React from "react";
+
+function index() {
+  return (
+    <>
+      {/* <div className="container mx-auto">
+        <div className="flex justify-center px-6 my-12">
+          <div className="w-full xl:w-3/4 lg:w-11/12 flex">
+            <div
+              className="w-full h-auto bg-gray-400 hidden lg:block lg:w-5/12 bg-cover rounded-l-lg"
+              style={{
+                backgroundImage:
+                  "url('https://images.unsplash.com/photo-1617195737496-bc30194e3a19?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80')",
+              }}
+            ></div>
+            <div className="w-full lg:w-7/12 bg-white p-5 rounded-lg lg:rounded-l-none">
+              <h3 className="pt-4 text-2xl text-center">Create an Account!</h3>
+              <form className="px-8 pt-6 pb-8 mb-4 bg-white rounded">
+                <div className="mb-4 md:flex md:justify-between">
+                  <div className="mb-4 md:mr-2 md:mb-0">
+                    <label
+                      className="block mb-2 text-sm font-bold text-gray-700"
+                      for="firstName"
+                    >
+                      First Name
+                    </label>
+                    <input
+                      className="w-full px-3 py-2 text-sm leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
+                      id="firstName"
+                      type="text"
+                      placeholder="First Name"
+                    />
+                  </div>
+                  <div className="md:ml-2">
+                    <label
+                      className="block mb-2 text-sm font-bold text-gray-700"
+                      for="lastName"
+                    >
+                      Last Name
+                    </label>
+                    <input
+                      className="w-full px-3 py-2 text-sm leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
+                      id="lastName"
+                      type="text"
+                      placeholder="Last Name"
+                    />
+                  </div>
+                </div>
+                <div className="mb-4">
+                  <label
+                    className="block mb-2 text-sm font-bold text-gray-700"
+                    for="email"
+                  >
+                    Email
+                  </label>
+                  <input
+                    className="w-full px-3 py-2 mb-3 text-sm leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
+                    id="email"
+                    type="email"
+                    placeholder="Email"
+                  />
+                </div>
+                <div className="mb-4 md:flex md:justify-between">
+                  <div className="mb-4 md:mr-2 md:mb-0">
+                    <label
+                      className="block mb-2 text-sm font-bold text-gray-700"
+                      for="password"
+                    >
+                      Password
+                    </label>
+                    <input
+                      className="w-full px-3 py-2 mb-3 text-sm leading-tight text-gray-700 border  rounded shadow appearance-none focus:outline-none focus:shadow-outline"
+                      id="password"
+                      type="password"
+                      placeholder="******************"
+                    />
+                  </div>
+                  <div className="md:ml-2">
+                    <label
+                      className="block mb-2 text-sm font-bold text-gray-700"
+                      for="c_password"
+                    >
+                      Confirm Password
+                    </label>
+                    <input
+                      className="w-full px-3 py-2 mb-3 text-sm leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
+                      id="c_password"
+                      type="password"
+                      placeholder="******************"
+                    />
+                  </div>
+                </div>
+                <div className="mb-6 text-center">
+                  <button
+                    className="w-full px-4 py-2 font-bold text-white bg-blue-500 rounded-full hover:bg-blue-700 focus:outline-none focus:shadow-outline"
+                    type="button"
+                  >
+                    Register Account
+                  </button>
+                </div>
+                <hr className="mb-6 border-t" />
+                <div className="text-center">
+                  <a
+                    className="inline-block text-sm text-blue-500 align-baseline hover:text-blue-800"
+                    href="#"
+                  >
+                    Forgot Password?
+                  </a>
+                </div>
+                <div className="text-center">
+                  <Link
+                    className="inline-block text-sm text-blue-500 align-baseline hover:text-blue-800"
+                    href="/login"
+                  >
+                    Already have an account? Login!
+                  </Link>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div> */}
+      <section className="bg-white">
+        <div className="lg:grid lg:min-h-screen lg:grid-cols-12">
+          <section className="relative flex h-32 items-end bg-gray-900 lg:col-span-5 lg:h-full xl:col-span-6">
+            <img
+              alt="Night"
+              src="https://images.unsplash.com/photo-1617195737496-bc30194e3a19?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
+              className="absolute inset-0 h-full w-full object-cover opacity-80"
+            />
+
+            <div className="hidden lg:relative lg:block lg:p-12">
+              <a className="block text-white" href="/">
+                <img
+                  src="paysoko.png"
+                  className="h-6 mr-3 sm:h-9"
+                  alt="Paysoko Logo"
+                />
+              </a>
+
+              <h2 className="mt-6 text-2xl font-bold text-white sm:text-3xl md:text-4xl">
+                Welcome to SinglePage
+              </h2>
+
+              <p className="mt-4 leading-relaxed text-white/90">
+                PaySoko Payments and eBusiness solutions gives your brand access
+                to modern commerce and modern digital payments solutions,
+                offering your businesses new opportunities for growth and
+                success.
+              </p>
+            </div>
+          </section>
+
+          <main
+            aria-label="Main"
+            className="flex items-center justify-center px-8 py-8 sm:px-12 lg:col-span-7 lg:py-12 lg:px-16 xl:col-span-6"
+          >
+            <div className="max-w-xl lg:max-w-3xl">
+              <div className="relative -mt-16 block lg:hidden">
+                <a
+                  className="inline-flex h-16 w-16 items-center justify-center rounded-full bg-white text-blue-600 sm:h-20 sm:w-20"
+                  href="/"
+                >
+                  <img
+                    src="paysoko.png"
+                    className="h-6 mr-3 sm:h-9"
+                    alt="Paysoko Logo"
+                  />
+                </a>
+
+                <h1 className="mt-2 text-2xl font-bold text-gray-900 sm:text-3xl md:text-4xl">
+                  Welcome to SinglePage
+                </h1>
+
+                <p className="mt-4 leading-relaxed text-gray-500">
+                  PaySoko Payments and eBusiness solutions gives your brand
+                  access to modern commerce and modern digital payments
+                  solutions, offering your businesses new opportunities for
+                  growth and success.
+                </p>
+              </div>
+
+              <form action="#" className="mt-8 grid grid-cols-6 gap-6">
+                <div className="col-span-6 sm:col-span-3">
+                  <label
+                    htmlFor="FirstName"
+                    className="block text-sm font-medium text-gray-700"
+                  >
+                    First Name
+                  </label>
+
+                  <input
+                    type="text"
+                    id="FirstName"
+                    name="first_name"
+                    className="mt-1 w-full p-2 rounded-md border border-gray-100 bg-white text-sm text-gray-700 shadow-sm"
+                  />
+                </div>
+
+                <div className="col-span-6 sm:col-span-3">
+                  <label
+                    htmlFor="LastName"
+                    className="block text-sm font-medium text-gray-700"
+                  >
+                    Last Name
+                  </label>
+
+                  <input
+                    type="text"
+                    id="LastName"
+                    name="last_name"
+                    className="mt-1 w-full p-2 rounded-md border border-gray-100 bg-white text-sm text-gray-700 shadow-sm"
+                  />
+                </div>
+
+                <div className="col-span-6">
+                  <label
+                    htmlFor="Email"
+                    className="block text-sm font-medium text-gray-700"
+                  >
+                    Email
+                  </label>
+
+                  <input
+                    type="email"
+                    id="Email"
+                    name="email"
+                    className="mt-1 w-full p-2 rounded-md border border-gray-100 bg-white text-sm text-gray-700 shadow-sm"
+                  />
+                </div>
+
+                <div className="col-span-6 sm:col-span-3">
+                  <label
+                    htmlFor="Password"
+                    className="block text-sm font-medium text-gray-700"
+                  >
+                    Password
+                  </label>
+
+                  <input
+                    type="password"
+                    id="Password"
+                    name="password"
+                    className="mt-1 w-full p-2 rounded-md border border-gray-100 bg-white text-sm text-gray-700 shadow-sm"
+                  />
+                </div>
+
+                <div className="col-span-6 sm:col-span-3">
+                  <label
+                    htmlFor="PasswordConfirmation"
+                    className="block text-sm font-medium text-gray-700"
+                  >
+                    Password Confirmation
+                  </label>
+
+                  <input
+                    type="password"
+                    id="PasswordConfirmation"
+                    name="password_confirmation"
+                    className="mt-1 w-full p-2 rounded-md border border-gray-100 bg-white text-sm text-gray-700 shadow-sm"
+                  />
+                </div>
+
+                <div className="col-span-6">
+                  <label htmlFor="MarketingAccept" className="flex gap-4">
+                    <input
+                      type="checkbox"
+                      id="MarketingAccept"
+                      name="marketing_accept"
+                      className="h-5 w-5 rounded-md border-gray-200 bg-white shadow-sm"
+                    />
+
+                    <span className="text-sm text-gray-700">
+                      I want to receive emails about events, product updates and
+                      company announcements.
+                    </span>
+                  </label>
+                </div>
+
+                <div className="col-span-6">
+                  <p className="text-sm text-gray-500">
+                    By creating an account, you agree to our
+                    <a href="#" className="text-gray-700 underline mx-1">
+                      terms and conditions
+                    </a>
+                    and
+                    <a href="#" className="text-gray-700 underline mx-1">
+                      privacy policy
+                    </a>
+                    .
+                  </p>
+                </div>
+
+                <div className="col-span-6 sm:flex sm:items-center sm:gap-4">
+                  <button className="inline-block shrink-0 rounded-md border border-blue-600 bg-blue-600 px-12 py-3 text-sm font-medium text-white transition hover:bg-transparent hover:text-blue-600 focus:outline-none focus:ring active:text-blue-500">
+                    Create an account
+                  </button>
+
+                  <Link
+                    href="/login"
+                    className="inline-block text-sm text-blue-500 align-baseline hover:text-blue-800"
+                  >
+                    Already have an account?Log in
+                  </Link>
+                </div>
+              </form>
+            </div>
+          </main>
+        </div>
+      </section>
+    </>
+  );
+}
+
+export default index;
